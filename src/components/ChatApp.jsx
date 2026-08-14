066d2f97e5c32dd90e5b35c121b8fb1843e61ced
Dev Dekode <dev.dekodeglobal@gmail.com>
2026-08-14 09:53:09 +0530

    Disable PlaneBanner temporarily

--- src/components/ChatApp.jsx
+++ src/components/ChatApp.jsx
@@ -16,7 +16,7 @@ import CompanyKnowledgePanel from "./CompanyKnowledgePanel";
 import ParticleBackground from "./ParticleBackground";
 import TypewriterText, { FormattedText } from "./TypewriterText";
 import HeroScenery from "./HeroScenery";
-import PlaneBanner from "./PlaneBanner";
+// import PlaneBanner from "./PlaneBanner";
 import DekodeVoiceEntry from "./voice/DekodeVoiceEntry";
 import DekodeVoiceSession from "./voice/DekodeVoiceSession";
 import MeetingScheduler from "./MeetingScheduler";
@@ -957,7 +957,7 @@ export default function ChatApp({
             timeOfDay={timeOfDay}
             realTime={realTime}
           />
-          <PlaneBanner timeOfDay={timeOfDay} />
+          {/* <PlaneBanner timeOfDay={timeOfDay} /> */}
         </>
       )}
 
